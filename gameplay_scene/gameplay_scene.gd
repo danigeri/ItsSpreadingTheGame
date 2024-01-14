91a8045810b6a7a4b4007847b5ebc8d8e1c3d49b
danigeri <danigerci@gmail.com>
2024-01-14 22:28:06 +0100

    Right truck can be steered.

--- gameplay_scene/gameplay_scene.gd
+++ gameplay_scene/gameplay_scene.gd
@@ -1,6 +1,20 @@
 extends Node2D
 
 @onready var game_over_scene : PackedScene = load("res://game_over_scene/game_over_scene.tscn")
+@onready var right_truck = $RoadScene/TruckScene
+
+var shift_speed : float = 0.0
+
 
 func _on_lose_button_pressed() -> void:
 	SceneTransition.change_scene(game_over_scene.instantiate())
+
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("ui_right"):
+		shift_speed += 0.1
+
+	if Input.is_action_pressed("ui_left"):
+		shift_speed -= 0.1
+
+	right_truck.translate(Vector2(10*shift_speed, 0))
