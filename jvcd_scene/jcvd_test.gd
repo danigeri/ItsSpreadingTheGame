6fa33a7aeaac2020747b7e256a33216be23e166f
danigeri <danigerci@gmail.com>
2024-01-16 17:15:06 +0100

    JVCD ragdol tester scene. Trying different physics

--- jvcd_scene/jcvd_test.gd
+++ jvcd_scene/jcvd_test.gd
@@ -1,1 +1,20 @@
 extends Node2D
+
+var shift_speed_left = 0
+var shift_speed_right = 0
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_right"):
+		shift_speed_left += 1
+
+	if Input.is_action_just_pressed("ui_left"):
+		shift_speed_left -= 1
+
+	if Input.is_action_just_pressed("LeftTruckToTheRight"):
+		shift_speed_right += 1
+
+	if Input.is_action_just_pressed("LeftTruckToTheLeft"):
+		shift_speed_right -= 1
+
+	$TestPlatform_L.move_and_collide(Vector2(shift_speed_left, 0))
+	$TestPlatform_R.move_and_collide(Vector2(shift_speed_right, 0))
