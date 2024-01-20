d0960b34be9d0afb4ad218528ee7223914ad6ece
danigeri <danigerci@gmail.com>
2024-01-20 12:21:40 +0100

    Jean head and torso lock rotation removed on falling.

--- jvcd_scene/jean.gd
+++ jvcd_scene/jean.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
 @onready var joints = $Joints
+@onready var torso = $Torso
+@onready var head = $Head
+
 
 func dismember() -> void:
 	if(joints):
 		remove_child($Joints)
+		torso.lock_rotation = false
+		head.lock_rotation = false
