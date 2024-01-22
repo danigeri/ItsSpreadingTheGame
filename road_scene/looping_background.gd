a53a1ef0daf0ca5120e76e9e2209b79b737ea132
danigeri <danigerci@gmail.com>
2024-01-22 21:57:23 +0100

    Smaller mountains added. Days are shorter. Sun and moon behaves differently.

--- road_scene/looping_background.gd
+++ road_scene/looping_background.gd
@@ -8,7 +8,7 @@ extends Node2D
 
 @onready var active_background = background
 
-@export var day_speed_multiplier : float = 1.0
+@export var day_speed_multiplier : float = 2.0
 
 const HEIGHT : int = 1067
 const DAYS_IN_PIXEL : float = HEIGHT/24
@@ -16,8 +16,8 @@ const DAYS_IN_PIXEL : float = HEIGHT/24
 
 func _process(delta: float) -> void:
 
-	background.position.y -= delta * 2.75 * day_speed_multiplier
-	background2.position.y -= delta * 2.75 * day_speed_multiplier
+	background.position.y -= delta * 2.5 * day_speed_multiplier
+	background2.position.y -= delta * 2.5 * day_speed_multiplier
 
 
 	# loop arund
@@ -32,19 +32,19 @@ func _process(delta: float) -> void:
 
 	var hour = abs(active_background.position.y)/DAYS_IN_PIXEL
 
-	if hour <= 1: # 0-1 (12-13): nothing
-		pass
-	elif hour > 1 and hour <= 6: # 1-6 (13-18): sun goes down
+	if hour <= 2: # 0-1 (12-13): nothing
+		sun.position.y -= delta * day_speed_multiplier
+	elif hour > 2 and hour <= 8: # 1-6 (13-18): sun goes down
 		sun.position.y += delta * day_speed_multiplier
-	elif hour > 6 and hour <= 7: # 6-7 (18-19): nothing
-		pass
-	elif hour > 7 and hour <= 12: # 7-12 (19-24): moon comes up
+#	elif hour > 6 and hour <= 7: # 6-7 (18-19): nothing
+#		pass
+	elif hour > 8 and hour <= 14: # 7-12 (19-24): moon comes up
 		moon.position.y -= delta * day_speed_multiplier
-	elif hour > 12 and hour <= 13: # 12-13 (24-1): nothing
-		pass
-	elif hour > 13 and hour <= 18: # 13-18 (1-6): moon goes down
+#	elif hour > 12 and hour <= 13: # 12-13 (24-1): nothing
+#		pass
+	elif hour > 14 and hour <= 20: # 13-18 (1-6): moon goes down
 		moon.position.y += delta * day_speed_multiplier
-	elif hour > 18 and hour <= 19: # 18-19 (6-7): nothing
-		pass
-	elif hour > 19 and hour <= 24: # 19-24 (7-12): sun comes up
+#	elif hour > 18 and hour <= 19: # 18-19 (6-7): nothing
+#		pass
+	elif hour > 20 and hour <= 24: # 19-24 (7-12): sun comes up
 		sun.position.y -= delta * day_speed_multiplier
