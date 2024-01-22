79eee55673530ad6fcc0b140053a75b94f0f4df3
Csaba Ekart <csaba.ekart@gmail.com>
2024-01-22 21:52:39 +0100

    logarithmic change

--- obstacle_scene/obstacle_scene.gd
+++ obstacle_scene/obstacle_scene.gd
@@ -6,14 +6,25 @@ var velocity_mps: float = -40.0 # This should be negative to move towards the pl
 var start_scale: Vector2 = Vector2(1, 1) # The scale at the bottom of the screen
 var horizon_scale: Vector2 = Vector2(0, 0) # The scale at the horizon
 
+var min_scale: float = 0.1 # The minimum scale when the obstacle reaches the horizon
+var max_scale: float = 1.0 # The maximum scale when the obstacle is at the bottom
+
 func _physics_process(delta: float) -> void:
 	position.y += velocity_mps * delta
 	# sprite_2d.scale *= 0.999
 	# todo remove from screen
+	
 	var road_scene = get_parent()
 	var horizon_y = road_scene.get_horizon_y_position()
-	#var scale_factor = lerp(1.0, 0.0, (position.y - horizon_y) / (road_scene.HEIGHT - horizon_y))
-	var scale_factor = lerp(1.0, 0.0, (road_scene.HEIGHT - position.y) / (road_scene.HEIGHT - horizon_y))
-	scale = start_scale * scale_factor
-	if position.y < horizon_y:
-		queue_free() # Remove the obstacle if it's reached the horizon
+	# Clamp the position to avoid going beyond the horizon or below the screen bottom
+	position.y = clamp(position.y, horizon_y, road_scene.HEIGHT)
+	# Calculate the scale factor based on the logarithmic scale
+	# The +1 ensures we never try to take the logarithm of 0.
+	var distance_ratio = (position.y - horizon_y) / (road_scene.HEIGHT - horizon_y)
+	var scale_factor = log(distance_ratio * (1 / min_scale - 1) + 1) / log(1 / min_scale)
+
+	scale = Vector2(max_scale, max_scale) * scale_factor
+	
+	# If the obstacle has reached the horizon, remove it from the scene
+	if position.y <= horizon_y:
+		queue_free()
