45aed6f1553b5985b7b87ccd59b223a5fcfa2acf
Csaba Ekart <csaba.ekart@gmail.com>
2024-01-26 23:10:22 +0100

    intro szuszogas

--- menu_scene/menu_scene.gd
+++ menu_scene/menu_scene.gd
@@ -1,7 +1,8 @@
 extends Control
 
 func _ready():
-	BackgroundMusicPlayer.play()
+	#BackgroundMusicPlayer.play()
+	pass
 
 func _on_start_button_pressed() -> void:
 	var gameplay_scene = preload("res://gameplay_scene/gameplay_scene.tscn").instantiate()
