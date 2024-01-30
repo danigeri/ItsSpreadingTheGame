9cb3999142805d4f6218e0d1f22c07ee147f81fe
danigeri <danigerci@gmail.com>
2024-01-30 18:00:49 +0100

    Last minute fixes.

--- menu_scene/menu_scene.gd
+++ menu_scene/menu_scene.gd
@@ -1,17 +1,15 @@
 extends Control
 
-func _ready():
-	#BackgroundMusicPlayer.play()
-	pass
 
 func _on_start_button_pressed() -> void:
 	# todo only once need to enter name
 	var enter_username_scene = preload("res://enter_username_scene/enter_username_scene.tscn").instantiate()
-	SceneTransition.change_scene(enter_username_scene)
-	
+	var current_scene = get_tree().root.get_child(5)
+	current_scene.queue_free()
+	get_tree().root.add_child(enter_username_scene)
+
 
 func _on_high_score_button_pressed():
 	print("scoreboard button todo")
 	var leaderboard_scene = preload("res://leaderboard_scene/leaderboard_scene.tscn").instantiate()
 	SceneTransition.change_scene(leaderboard_scene)
-	pass # Replace with function body.
